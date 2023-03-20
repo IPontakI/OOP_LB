145b8f402f87662fc10275d8b3bf302257cea5bc
IPontakI <valik_bilokin@ukr.net>
2023-03-20 22:28:22 +0200

    labaremake_to_form

---
+++ LB_04/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPrintArr = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblMultiplication = new System.Windows.Forms.Label();
+            this.lblSumm = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblPrintPairArr = new System.Windows.Forms.Label();
+            this.lblPrintPairArrSorted = new System.Windows.Forms.Label();
+            this.lblPrintOddArrSorted = new System.Windows.Forms.Label();
+            this.lblPrintOddArr = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblRightDownCorner = new System.Windows.Forms.Label();
+            this.lblLeftUpCorner = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(713, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(607, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Array: ";
+            // 
+            // lblPrintArr
+            // 
+            this.lblPrintArr.AutoSize = true;
+            this.lblPrintArr.Location = new System.Drawing.Point(13, 26);
+            this.lblPrintArr.Name = "lblPrintArr";
+            this.lblPrintArr.Size = new System.Drawing.Size(35, 13);
+            this.lblPrintArr.TabIndex = 4;
+            this.lblPrintArr.Text = "label2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "First task a)";
+            // 
+            // lblMultiplication
+            // 
+            this.lblMultiplication.AutoSize = true;
+            this.lblMultiplication.Location = new System.Drawing.Point(12, 77);
+            this.lblMultiplication.Name = "lblMultiplication";
+            this.lblMultiplication.Size = new System.Drawing.Size(35, 13);
+            this.lblMultiplication.TabIndex = 6;
+            this.lblMultiplication.Text = "label3";
+            // 
+            // lblSumm
+            // 
+            this.lblSumm.AutoSize = true;
+            this.lblSumm.Location = new System.Drawing.Point(11, 118);
+            this.lblSumm.Name = "lblSumm";
+            this.lblSumm.Size = new System.Drawing.Size(35, 13);
+            this.lblSumm.TabIndex = 8;
+            this.lblSumm.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "First task b)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(192, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Sorting";
+            // 
+            // lblPrintPairArr
+            // 
+            this.lblPrintPairArr.AutoSize = true;
+            this.lblPrintPairArr.Location = new System.Drawing.Point(197, 37);
+            this.lblPrintPairArr.Name = "lblPrintPairArr";
+            this.lblPrintPairArr.Size = new System.Drawing.Size(35, 13);
+            this.lblPrintPairArr.TabIndex = 10;
+            this.lblPrintPairArr.Text = "label5";
+            // 
+            // lblPrintPairArrSorted
+            // 
+            this.lblPrintPairArrSorted.AutoSize = true;
+            this.lblPrintPairArrSorted.Location = new System.Drawing.Point(197, 61);
+            this.lblPrintPairArrSorted.Name = "lblPrintPairArrSorted";
+            this.lblPrintPairArrSorted.Size = new System.Drawing.Size(35, 13);
+            this.lblPrintPairArrSorted.TabIndex = 11;
+            this.lblPrintPairArrSorted.Text = "label5";
+            // 
+            // lblPrintOddArrSorted
+            // 
+            this.lblPrintOddArrSorted.AutoSize = true;
+            this.lblPrintOddArrSorted.Location = new System.Drawing.Point(197, 110);
+            this.lblPrintOddArrSorted.Name = "lblPrintOddArrSorted";
+            this.lblPrintOddArrSorted.Size = new System.Drawing.Size(35, 13);
+            this.lblPrintOddArrSorted.TabIndex = 13;
+            this.lblPrintOddArrSorted.Text = "label5";
+            // 
+            // lblPrintOddArr
+            // 
+            this.lblPrintOddArr.AutoSize = true;
+            this.lblPrintOddArr.Location = new System.Drawing.Point(197, 86);
+            this.lblPrintOddArr.Name = "lblPrintOddArr";
+            this.lblPrintOddArr.Size = new System.Drawing.Size(35, 13);
+            this.lblPrintOddArr.TabIndex = 12;
+            this.lblPrintOddArr.Text = "label5";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Second task 1)";
+            // 
+            // lblRightDownCorner
+            // 
+            this.lblRightDownCorner.AutoSize = true;
+            this.lblRightDownCorner.Location = new System.Drawing.Point(144, 169);
+            this.lblRightDownCorner.Name = "lblRightDownCorner";
+            this.lblRightDownCorner.Size = new System.Drawing.Size(35, 13);
+            this.lblRightDownCorner.TabIndex = 15;
+            this.lblRightDownCorner.Text = "label6";
+            // 
+            // lblLeftUpCorner
+            // 
+            this.lblLeftUpCorner.AutoSize = true;
+            this.lblLeftUpCorner.Location = new System.Drawing.Point(144, 199);
+            this.lblLeftUpCorner.Name = "lblLeftUpCorner";
+            this.lblLeftUpCorner.Size = new System.Drawing.Size(35, 13);
+            this.lblLeftUpCorner.TabIndex = 16;
+            this.lblLeftUpCorner.Text = "label6";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(14, 268);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Обробити";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.ControlLightLight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.Location = new System.Drawing.Point(14, 159);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.Size = new System.Drawing.Size(124, 110);
+            this.dataGridView1.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 299);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.lblLeftUpCorner);
+            this.Controls.Add(this.lblRightDownCorner);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblPrintOddArrSorted);
+            this.Controls.Add(this.lblPrintOddArr);
+            this.Controls.Add(this.lblPrintPairArrSorted);
+            this.Controls.Add(this.lblPrintPairArr);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblSumm);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblMultiplication);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblPrintArr);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPrintArr;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblMultiplication;
+        private System.Windows.Forms.Label lblSumm;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblPrintPairArr;
+        private System.Windows.Forms.Label lblPrintPairArrSorted;
+        private System.Windows.Forms.Label lblPrintOddArrSorted;
+        private System.Windows.Forms.Label lblPrintOddArr;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblRightDownCorner;
+        private System.Windows.Forms.Label lblLeftUpCorner;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ LB_04/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.ColumnHeadersVisible = false;
+            dataGridView1.RowHeadersVisible = false;
+
+            lblPrintArr.Text = "";
+            lblMultiplication.Text = "";
+            lblSumm.Text = "";
+            lblPrintPairArr.Text = "";
+            lblPrintPairArrSorted.Text = "";
+            lblPrintOddArr.Text = "";
+            lblPrintOddArrSorted.Text = "";
+            lblRightDownCorner.Text = "";
+            lblLeftUpCorner.Text = "";
+
+            //dataGridView1.Visible = false;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+            double[] array = new double[10];
+            string arrStr = "";
+            string arrStr1 = "";
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(-20, 30);
+                arrStr += array[i].ToString() + " ";
+            }
+            //Console.WriteLine("Array:");
+            lblPrintArr.Text = arrStr;
+            arrStr = "";
+
+            //Console.WriteLine("\tFirst task a)");
+            double summ = 1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0)
+                {
+                    summ *= array[i];
+                }
+            }
+            //Console.WriteLine("Summ value = " + summ);
+            lblMultiplication.Text = "Summ value = " + summ;
+
+            //Console.WriteLine("\n\tFirst task b)");
+            double minEl = array[0];
+            summ = 0;
+            int minIndex = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (minEl > array[i])
+                {
+                    minEl = array[i];
+                    minIndex = i;
+                }
+            }
+
+            for (int i = 0; i < minIndex; i++)
+            {
+                summ += array[i];
+            }
+            lblSumm.Text = "Summ value = " + summ;
+
+            //Console.WriteLine("\n\tSorting");
+
+            double[] arrPair = new double[10];
+            
+            double[] arrOdd = new double[10];
+            
+            for (int i = 0, p = 0, o = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                {                    
+                    arrOdd[o] = array[i];
+                    arrStr1 += arrOdd[o] + " ";
+                    o++;
+                }
+                else
+                {
+                    arrPair[p] = array[i];
+                    arrStr += arrPair[p] + " ";
+                    p++;
+                }
+            }
+
+            //Console.WriteLine("Pair arr:");
+            lblPrintPairArr.Text = "Pair arr: " + arrStr;
+            //Console.WriteLine("Pair arr sorted:");
+            arrStr = "";
+            Array.Sort(arrPair);            
+            for(int i = 0; i < arrPair.Length; i++)
+            {
+                if (arrPair[i] != 0)
+                {
+                    arrStr += arrPair[i] + " ";
+                }
+            }
+            lblPrintPairArrSorted.Text = "Pair arr sorted: " + arrStr;
+
+
+            //Console.WriteLine("Odd arr:");
+            lblPrintOddArr.Text = "Odd arr: " + arrStr1;
+            //Console.WriteLine("Odd arr sorted:");
+            arrStr1 = "";
+            Array.Sort(arrOdd);            
+            for (int i = 0; i < arrOdd.Length; i++)
+            {
+                if(arrOdd[i] != 0)
+                {
+                    arrStr1 += arrOdd[i] + " ";
+                }                
+            }
+            lblPrintOddArrSorted.Text = "Odd arr sorted: " + arrStr1;
+            //--------------------------------------------
+            //Console.WriteLine("\tSecond task 1)");
+
+            int[,] array2 = new int[5, 5];
+            dataGridView1.RowCount = 5;
+            dataGridView1.ColumnCount = 5;
+
+            
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    array2[i, j] = rnd.Next(-20, 30);
+                    dataGridView1.Rows[i].Cells[j].Value = array2[i, j].ToString();
+                }
+            }        
+                    
+            lblRightDownCorner.Text = "Right-down corner: \t" + array2[4, 4];
+
+            lblLeftUpCorner.Text = "Left-up corner: \t" + array2[0, 0];
+        }
+    }
+}
