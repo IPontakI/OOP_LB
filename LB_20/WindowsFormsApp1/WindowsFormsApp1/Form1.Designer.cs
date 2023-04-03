933db6c78aad49a68040c3756f56b55f9f9f3a4d
IPontakI <valik_bilokin@ukr.net>
2023-04-03 19:28:20 +0300

    LB_20

---
+++ LB_20/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbGetA = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tbGetB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbGetA
+            // 
+            this.tbGetA.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbGetA.Location = new System.Drawing.Point(26, 46);
+            this.tbGetA.Name = "tbGetA";
+            this.tbGetA.Size = new System.Drawing.Size(121, 29);
+            this.tbGetA.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.comboBox1.Location = new System.Drawing.Point(153, 43);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(53, 32);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // tbGetB
+            // 
+            this.tbGetB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbGetB.Location = new System.Drawing.Point(258, 46);
+            this.tbGetB.Name = "tbGetB";
+            this.tbGetB.Size = new System.Drawing.Size(121, 29);
+            this.tbGetB.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(212, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "X =";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.button1.Location = new System.Drawing.Point(26, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(353, 38);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Click";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lblAnswer.Location = new System.Drawing.Point(22, 78);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(60, 24);
+            this.lblAnswer.TabIndex = 5;
+            this.lblAnswer.Text = "label2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(25, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Enter A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(254, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Enter B";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 167);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbGetB);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.tbGetA);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbGetA;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox tbGetB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ LB_20/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblAnswer.Text = "Answer: ";
+            comboBox1.SelectedIndex = 0;
+
+            tbGetA.MaxLength = 5;            
+            tbGetB.MaxLength = 5;            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double answer;
+            double a;
+            double b;
+
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    {
+                        try
+                        {
+                            a = Convert.ToDouble(tbGetA.Text);
+                            b = Convert.ToDouble(tbGetB.Text);
+                            answer = b - a;
+                            lblAnswer.Text = "Answer: X = " + answer.ToString();
+                            tbGetA.Text = "";
+                            tbGetB.Text = "";
+                        }
+                        catch (Exception exp)
+                        {
+                            MessageBox.Show(exp.Message, "Exception message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            tbGetA.Text = "";
+                            tbGetB.Text = "";
+                        }
+                        break;
+                    }
+                case 1:
+                    {
+                        try
+                        {
+                            a = Convert.ToDouble(tbGetA.Text);
+                            b = Convert.ToDouble(tbGetB.Text);
+                            answer = -(b - a);
+                            lblAnswer.Text = "Answer: X = " + answer.ToString();
+                            tbGetA.Text = "";
+                            tbGetB.Text = "";
+                        }
+                        catch (Exception exp)
+                        {
+                            MessageBox.Show(exp.Message, "Exception message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            tbGetA.Text = "";
+                            tbGetB.Text = "";
+                        }
+                        break;
+                    }
+                case 2:
+                    {                        
+                        try
+                        {
+                            a = Convert.ToDouble(tbGetA.Text);
+                            b = Convert.ToDouble(tbGetB.Text);
+                            answer = b / a;
+                            lblAnswer.Text = "Answer: X = " + answer.ToString();
+                            tbGetA.Text = "";
+                            tbGetB.Text = "";
+                        }
+                        catch (Exception exp)
+                        {
+                            MessageBox.Show(exp.Message, "Exception message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            tbGetA.Text = "";
+                            tbGetB.Text = "";
+                        }
+                        break;
+                    }
+                case 3:
+                    {
+                        try
+                        {
+                            a = Convert.ToDouble(tbGetA.Text);
+                            b = Convert.ToDouble(tbGetB.Text);
+                            if(a == 0)
+                            {
+                                throw new Exception("Ви намагаєтеся виконати ділення на нуль.");
+                            }
+                            if (b == 0)
+                            {
+                                throw new Exception("X не може дорівнювати нулю.");
+                            }
+                            answer = b * a;
+                            lblAnswer.Text = "Answer: X = " + answer.ToString();
+                            tbGetA.Text = "";
+                            tbGetB.Text = "";
+                        }
+                        catch (Exception exp)
+                        {
+                            MessageBox.Show(exp.Message, "Exception message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            tbGetA.Text = "";
+                            tbGetB.Text = "";
+                        }
+                        break;
+                    }
+            }                      
+        }
+    }
+}
