ae003e9b8704c561bc1d6ba14e2e72779568c77e
IPontakI <valik_bilokin@ukr.net>
2023-05-23 11:18:21 +0300

    LB_24

---
+++ LB_24/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Green;
+            this.button1.Location = new System.Drawing.Point(152, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Почати 1й потік ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(152, 291);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Зупинити 1й потік";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.Color.Green;
+            this.button3.Location = new System.Drawing.Point(322, 266);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Почати 2й потік";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.ForeColor = System.Drawing.Color.Red;
+            this.button4.Location = new System.Drawing.Point(322, 291);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(109, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Зупинити 2й потік";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.ForeColor = System.Drawing.Color.Green;
+            this.button5.Location = new System.Drawing.Point(492, 266);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(109, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Почати 2й потік";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.ForeColor = System.Drawing.Color.Red;
+            this.button6.Location = new System.Drawing.Point(492, 291);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(109, 23);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "Зупинити 2й потік";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(152, 167);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(149, 212);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(319, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "label2";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(322, 167);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(489, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "label3";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(492, 167);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 11;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button7
+            // 
+            this.button7.ForeColor = System.Drawing.Color.Green;
+            this.button7.Location = new System.Drawing.Point(258, 335);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(242, 23);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "Запустити всі потоки";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.ForeColor = System.Drawing.Color.Red;
+            this.button8.Location = new System.Drawing.Point(258, 360);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(242, 23);
+            this.button8.TabIndex = 13;
+            this.button8.Text = "Зупинити всі потоки";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ LB_24/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Thread thread1;
+        Thread thread2;
+        Thread thread3;
+
+        string input;
+
+        public Form1()
+        {
+            InitializeComponent();
+            thread1 = new Thread(new ThreadStart(fu));
+            thread2 = new Thread(new ThreadStart(fu2));
+            thread3 = new Thread(new ThreadStart(fu3));
+        }
+
+        public void fu() 
+        {
+            Encrypt(plaintext, key, encryptedText);                
+        }
+
+        string hashedValue;
+
+        public void fu2()
+        {
+            hashedValue = ComputeHash(input, 8);
+        }
+
+        byte[] encryptedData;
+
+        public void fu3()
+        {
+            var rsa = new RSACryptoServiceProvider();
+            var publicKey = rsa.ExportParameters(false);
+            encryptedData = Encrypt2(Encoding.UTF8.GetBytes(textBox3.Text), publicKey);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+
+            button3.Enabled = false;
+            button4.Enabled = false;
+
+            button5.Enabled = false;
+            button6.Enabled = false;
+
+            button7.Enabled = false;
+            button8.Enabled = false;
+
+            label1.Text = "";
+            label2.Text = "";
+            label3.Text = "";
+        }
+
+        private static int[] sBox = new int[8] { 5, 2, 1, 6, 3, 4, 7, 0 };
+
+        public static void Encrypt(string plaintext, string key, string encryptedText)
+        {
+            // Перевірка валідності ключа та тексту
+            if (string.IsNullOrEmpty(plaintext) || string.IsNullOrEmpty(key))
+            {
+                throw new ArgumentException("Пустий текст або ключ.");
+            }
+
+            // Доповнення ключа
+            if (key.Length < 8)
+            {
+                key = key.PadRight(8, '0');
+            }
+            else if (key.Length > 8)
+            {
+                key = key.Substring(0, 8);
+            }
+
+            // Шифрування
+            int[] block = new int[8];
+            string ciphertext = string.Empty;
+
+            for (int i = 0; i < plaintext.Length; i += 8)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (i + j < plaintext.Length)
+                    {
+                        block[j] = (int)plaintext[i + j];
+                    }
+                    else
+                    {
+                        block[j] = 0; // Доповнення нулями
+                    }
+                }
+
+                int[] roundKey = GenerateRoundKey(key);
+                for (int round = 0; round < 16; round++)
+                {
+                    block = RoundFunction(block, roundKey);
+                    roundKey = GenerateRoundKey(roundKey.ToString());
+                }
+
+                for (int j = 0; j < 8; j++)
+                {
+                    ciphertext += (char)block[j];
+                }
+            }
+            encryptedText = ciphertext;
+
+            //return ciphertext;
+        }
+
+        private static int[] GenerateRoundKey(string key)
+        {
+            int[] roundKey = new int[8];
+            for (int i = 0; i < 8; i++)
+            {
+                roundKey[i] = (int)key[i];
+            }
+
+            // Логічне сумування з ключем
+            roundKey[0] ^= 1;
+            roundKey[1] ^= 2;
+            roundKey[2] ^= 4;
+            roundKey[3] ^= 8;
+            roundKey[4] ^= 16;
+            roundKey[5] ^= 32;
+            roundKey[6] ^= 64;
+            roundKey[7] ^= 128;
+
+            return roundKey;
+        }
+
+        private static int[] RoundFunction(int[] block, int[] roundKey)
+        {
+            // Логічне сумування з ключем
+            for (int i = 0; i < 8; i++)
+            {
+                block[i] ^= roundKey[i];
+            }           
+            
+            // Застосування S-блоку
+            for (int i = 0; i < 8; i++)
+            {
+               //block[i] = sBox[block[i]-1];
+            }
+
+            // Зсув ліворуч
+            int temp = block[0];
+            for (int i = 0; i < 7; i++)
+            {
+                block[i] = block[i + 1];
+            }
+            block[7] = temp;
+
+            return block;
+        }
+        string plaintext = "Hello, world!";
+        string key = "mysecret";
+        string encryptedText;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //thread1.Start();
+            Encrypt(plaintext, key, encryptedText);
+            label1.Text = encryptedText;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            thread1.Suspend();
+        }
+        public static string ComputeHash(string input, int n)
+        {
+            using (var algorithm = SHA256.Create()) // Використовуйте вибраний алгоритм хешування
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = algorithm.ComputeHash(inputBytes);
+
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < n; i++)
+                {
+                    stringBuilder.Append(hashBytes[i].ToString("X2")); // Представлення хешу в шістнадцятковому форматі
+                }
+
+                return stringBuilder.ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //thread2.Start();
+            hashedValue = ComputeHash(input, 8);
+            input = textBox2.Text;            
+            label2.Text = hashedValue;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            thread2.Suspend();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+
+                button7.Enabled = false;
+                button8.Enabled = false;
+            }
+            else
+            {
+                button1.Enabled = true;
+                button2.Enabled = true;
+
+                if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
+                {
+                    button7.Enabled = true;
+                    button8.Enabled = true;
+                }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "")
+            {
+                button3.Enabled = false;
+                button4.Enabled = false;
+
+                button7.Enabled = false;
+                button8.Enabled = false;
+            }
+            else
+            {
+                button3.Enabled = true;
+                button4.Enabled = true;
+
+                if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
+                {
+                    button7.Enabled = true;
+                    button8.Enabled = true;
+                }
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox3.Text == "")
+            {
+                button5.Enabled = false;
+                button6.Enabled = false;
+
+                button7.Enabled = false;
+                button8.Enabled = false;
+            }
+            else
+            {
+                button5.Enabled = true;
+                button6.Enabled = true;
+
+                if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
+                {
+                    button7.Enabled = true;
+                    button8.Enabled = true;
+                }
+            }           
+        }
+
+        public static byte[] Encrypt2(byte[] data, RSAParameters publicKey)
+        {
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                rsa.ImportParameters(publicKey);
+                return rsa.Encrypt(data, true);
+            }
+        }        
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //thread3.Start();
+            var rsa = new RSACryptoServiceProvider();
+            var publicKey = rsa.ExportParameters(false);
+            encryptedData = Encrypt2(Encoding.UTF8.GetBytes(textBox3.Text), publicKey);
+
+            label3.Text = Convert.ToBase64String(encryptedData);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            thread3.Suspend();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            thread1.Suspend();
+            thread2.Suspend();
+            thread3.Suspend();
+        }
+    }
+}
