721d1f9e44edec2a38aeb5a5b4f245de6905e72b
IPontakI <valik_bilokin@ukr.net>
2023-06-03 15:57:25 +0300

    LB_26

---
+++ LB_26/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.button1.Location = new System.Drawing.Point(254, 388);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(322, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Invite";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.textBox1.Location = new System.Drawing.Point(376, 265);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 38);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.textBox2.Location = new System.Drawing.Point(376, 306);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 38);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.textBox4.Location = new System.Drawing.Point(376, 388);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(200, 38);
+            this.textBox4.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(255, 262);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 31);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label2.Location = new System.Drawing.Point(255, 306);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 31);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Invitatior";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label3.Location = new System.Drawing.Point(255, 347);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 31);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Time";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label4.Location = new System.Drawing.Point(255, 388);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 31);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Place";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(327, 190);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(176, 42);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Invitation";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(376, 350);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 29);
+            this.dateTimePicker1.TabIndex = 11;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(237, 447);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(191, 28);
+            this.radioButton1.TabIndex = 12;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Invitation template 1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(434, 447);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(191, 28);
+            this.radioButton2.TabIndex = 13;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Invitation template 2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(254, 482);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(322, 41);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "View template";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(749, 579);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ LB_26/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Word.Application word = new Word.Application();
+        Word.Document doc = new Word.Document();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            radioButton1.Select();            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                string fn = @"D:\!Валик\Универ\ООП\c#\LB_26\WindowsFormsApp1\WindowsFormsApp1\bin\Debug\Invitation.dotx";
+
+                Object missingObj = System.Reflection.Missing.Value;
+                Object trueObj = true;
+                Object falseObj = false;
+                Object templatePathObj = fn;
+
+                doc = word.Documents.Add(ref templatePathObj, ref missingObj, ref missingObj, ref missingObj);
+                doc.Activate();
+                try
+                {
+                    foreach (Word.FormField f in doc.FormFields)
+                    {
+                        switch (f.Name)
+                        {
+                            case "mainName":
+                                f.Range.Text = textBox1.Text;
+                                break;
+                            case "invName":
+                                f.Range.Text = textBox2.Text;
+                                break;
+                            case "time":
+                                f.Range.Text = dateTimePicker1.Text;
+                                break;
+                            case "place":
+                                f.Range.Text = textBox4.Text;
+                                break;
+                            case "invName2":
+                                f.Range.Text = textBox2.Text;
+                                break;
+                        }
+                    }
+                    word.Visible = true;
+                }
+                catch (Exception error)
+                {
+                    doc.Close();
+                    word.Quit();
+                    doc = null;
+                    word = null;
+                    throw error;
+                }
+            }
+            else
+            {
+                string fn = @"D:\!Валик\Универ\ООП\c#\LB_26\WindowsFormsApp1\WindowsFormsApp1\bin\Debug\Invitation2.dotx";
+
+                Object missingObj = System.Reflection.Missing.Value;
+                Object trueObj = true;
+                Object falseObj = false;
+                Object templatePathObj = fn;
+
+                doc = word.Documents.Add(ref templatePathObj, ref missingObj, ref missingObj, ref missingObj);
+                doc.Activate();
+                try
+                {
+                    foreach (Word.FormField f in doc.FormFields)
+                    {
+                        switch (f.Name)
+                        {
+                            case "name":
+                                f.Range.Text = textBox1.Text;
+                                break;
+                            case "City":
+                                f.Range.Text = textBox2.Text;
+                                break;
+                            case "ТекстовоеПоле3":
+                                f.Range.Text = dateTimePicker1.Text;
+                                break;
+                            case "Name2":
+                                f.Range.Text = textBox1.Text;
+                                break;                            
+                        }
+                    }
+                    word.Visible = true;
+                }
+                catch (Exception error)
+                {
+                    doc.Close();
+                    word.Quit();
+                    doc = null;
+                    word = null;
+                    throw error;
+                }
+            }           
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            label2.Text = "Invitator";
+
+            label1.Location = new Point(418, 344);//+
+            label2.Location = new Point(418, 388);//+
+            label3.Location = new Point(418, 429);//+
+            label4.Location = new Point(418, 470);//+
+            label4.Visible = true;
+            label5.Location = new Point(490, 272);//+
+
+            textBox1.Location = new Point(539, 347);
+            textBox2.Location = new Point(539, 388);
+
+            dateTimePicker1.Location = new Point(539, 432);
+
+            textBox4.Location = new Point(539, 470);
+            textBox4.Visible = true;
+
+            button1.Location = new Point(417, 520);
+            button2.Location = new Point(417, 614);
+
+            radioButton1.Location = new Point(400, 579);
+            radioButton2.Location = new Point(597, 579);
+
+            MaximumSize = new Size(1126, 700);
+            MinimumSize = new Size(1126, 700);
+
+            Image myimage = new Bitmap(@"D:\!Валик\Универ\ООП\c#\LB_26\Template\1.png");
+            this.BackgroundImage = myimage;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            label2.Text = "City";
+
+            label1.Location = new Point(255, 262);
+            label2.Location = new Point(255, 306);
+            label3.Location = new Point(255, 347);
+            label4.Location = new Point(418, 470);
+            label4.Visible = false;
+            label5.Location = new Point(327, 190);
+
+            textBox1.Location = new Point(376, 265);
+            textBox2.Location = new Point(376, 306);
+
+            dateTimePicker1.Location = new Point(376, 350);
+
+            textBox4.Location = new Point(539, 347);
+            textBox4.Visible = false;
+
+            button1.Location = new Point(254, 388);
+            button2.Location = new Point(254, 482); 
+
+            radioButton1.Location = new Point(237, 447);
+            radioButton2.Location = new Point(434, 447);
+
+            MaximumSize = new Size(765, 618);
+            MinimumSize = new Size(765, 618);
+
+            Image myimage = new Bitmap(@"D:\!Валик\Универ\ООП\c#\LB_26\Template\2.jpg");
+            this.BackgroundImage = myimage;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                Process.Start(@"D:\!Валик\Универ\ООП\c#\LB_26\WindowsFormsApp1\WindowsFormsApp1\bin\Debug\Invitation.dotx");
+            }
+            else
+            {
+                Process.Start(@"D:\!Валик\Универ\ООП\c#\LB_26\WindowsFormsApp1\WindowsFormsApp1\bin\Debug\Invitation2.dotx");
+            }
+            
+        }
+    }
+}
